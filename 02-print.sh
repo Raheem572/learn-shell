da199a3599b0bf39d7dc545eb3f33709c2b4db2d
raheembasha <raheembasha1993@gmail.com>
2024-01-25 16:06:07 +0530

    First Shell Script

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,10 @@
 # To  print a message
 
 echo "Hello world"
+
+echo -e "\e[31mHello in Red color\e[0m"
+echo -e "\e[32mHello in Green color\e[0m"
+echo -e "\e[33mHello in Yello color\e[0m"
+echo -e "\e34mHello in Blue color\e[0m"
+echo -e "\e35mHello in Magenta color\e[0m"
+echo -e "\e36mHello in Cyan color\e[0m"
