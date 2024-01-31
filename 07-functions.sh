bcf895eaa4ba7c891e89c7b362caac0b4000003b
raheem <raheem572@gmail.com>
2024-01-31 15:53:33 +0530

    Code added

--- 07-functions.sh
+++ 07-functions.sh
@@ -4,13 +4,16 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME/$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-       echo "$2 ......FAILURE"
+       echo "$2 ......$R FAILURE $N"
     else
-        echo "$2 ......SUCCESS"
+        echo "$2 ...... $G SUCCESS $N"
     fi
 
 }
