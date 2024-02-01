2e468b38e11b4106654f354e177251acdaf79e04
raheem <raheem572@gmail.com>
2024-02-01 13:47:35 +0530

    Code edited

--- 08-For-Loop.sh
+++ 08-For-Loop.sh
@@ -1,4 +1,45 @@
-for i in {1..100}
+#!bin/bash
+
+DATE=$(date +%F)
+LOGDIR=/home/centos/shellscript-logs
+SCRIPT_NAME=$0
+LOGFILE=$LOGDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 is ne 0 ]
+    then
+        echo -e "installing $2 .......$R Failure $N"
+    else
+        echo -e "installing $2 ....... $G Success $N"
+    fi
+
+}
+
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR::Please run this script with root access"
+    exit 1
+fi
+
+
+
+
+
+for i in $@
 do 
-    echo $i
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, lets install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed $N"
+    fi
 done
