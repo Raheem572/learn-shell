2990dca896f5e304094e11179fb72a6a8fb5c1d7
raheem <raheem572@gmail.com>
2024-02-05 16:37:34 +0530

    code edited

--- roboshop-shell/mongodb.sh
+++ roboshop-shell/mongodb.sh
@@ -23,7 +23,7 @@ VALIDATE(){
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR::Please run this script with root access"
+    echo -e "$R ERROR::Please run this script with root access $N"
     exit 1
 fi
 
