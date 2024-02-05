e9d71e1b28e2b120d2abc6cc9b5a1279fbc3b505
raheem <raheem572@gmail.com>
2024-02-05 11:54:04 +0530

    code edited

--- mail.sh
+++ mail.sh
@@ -7,3 +7,7 @@ TEAM_NAME=$4
 ALERT_TYPE=$5
 
 echo "all args: $@"
+
+FINAL_BODY=$(sed -e 's/TEAM_NAME/Devops Team/g' -e 's/ALERT_TYPE/High Disk Usage/g' -e "s/MESSAGE/$BODY" template.html)
+
+echo "$Final_BODY" | mail -s "$SUBJECT" "$TO_ADRESS"
